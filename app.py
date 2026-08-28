d2e21db38c8b1d98b5b393e2405159135f989c4e
valkol <koltsiuk_valeriy@kolgym.if.ua>
2026-08-28 13:54:43 +0300

    Begin post images

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+file = open("static/index.html", "r")
+html = file.read()
+file.close()
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello from backend!")
+
+        self.wfile.write(html.encode())
+
+    def do_POST(self):
+        # content_length = int(self.headers["Content-Length"])
+        # post_data = self.rfile.read(content_length)
+        # print(post_data.decode())
+
+        # self.send_response(200)
+        # self.end_headers()
+        # self.wfile.write(b"Data received")
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        
+        self.wfile.write(b"Ok")
 
 server=HTTPServer(("0.0.0.0", 8080), Handler)
-print("Server started on port 8080...")
 server.serve_forever()
